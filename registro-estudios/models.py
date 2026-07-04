b7b3e14e245961ecd2cec98b9f2843cc17786831
Alessa <203438900+Alessatabares@users.noreply.github.com>
2026-07-03 18:37:00 -0600

    Capa 2: modelo Registro y script para crear la tabla

---
+++ registro-estudios/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Registro(Base):
+    __tablename__ = "registros"
+    id = Column(Integer, primary_key=True)
+    paciente = Column(String)
+    protocolo = Column(String)
+    fecha = Column(String)
+    hora = Column(String)
+    doctor = Column(String)
