90bd079de95fa0679083d79954d5acbefc71cfc5
Alessa <203438900+Alessatabares@users.noreply.github.com>
2026-07-18 17:03:39 -0600

    Capa 4: función crear_token (genera JWT firmado)

--- registro-estudios/seguridad.py
+++ registro-estudios/seguridad.py
@@ -1,2 +1,10 @@
 SECRET_KEY = "2232df78caa11074ab9c5c38bd061511cf6733ae6cd4b3442fd7d575b3977404"
 ALGORITHM = "HS256"
+
+import jwt 
+from datetime import datetime, timedelta, timezone
+
+def crear_token(datos: dict):
+    datos["exp"] = (datetime.now(timezone.utc) + timedelta(minutes=30))
+    token = jwt.encode(datos, SECRET_KEY, algorithm=ALGORITHM)
+    return token
