a96627228e2e6485fb2fa0a283dd8255ba40e213
Alessa <203438900+Alessatabares@users.noreply.github.com>
2026-07-18 17:33:46 -0600

    Capa 4: función verificar_token (valida y rechaza tokens)

--- registro-estudios/seguridad.py
+++ registro-estudios/seguridad.py
@@ -2,9 +2,19 @@ SECRET_KEY = "2232df78caa11074ab9c5c38bd061511cf6733ae6cd4b3442fd7d575b3977404"
 ALGORITHM = "HS256"
 
 import jwt 
+
+from fastapi import HTTPException
 from datetime import datetime, timedelta, timezone
 
 def crear_token(datos: dict):
     datos["exp"] = (datetime.now(timezone.utc) + timedelta(minutes=30))
     token = jwt.encode(datos, SECRET_KEY, algorithm=ALGORITHM)
     return token
+
+def verificar_token(token: str):
+    try: 
+        datos = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return datos
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="token invalido")
+    
